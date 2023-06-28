a4027f3e7188183df3fbc074b7230b90ee7c68f4
danys <danys@PC-di-Daniela.Home>
2023-06-28 15:00:19 +0200

    .

--- src/main/java/it/polito/tdp/nyc/FXMLController.java
+++ src/main/java/it/polito/tdp/nyc/FXMLController.java
@@ -2,10 +2,12 @@ package it.polito.tdp.nyc;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.nyc.model.Arco;
 import it.polito.tdp.nyc.model.Model;
+import it.polito.tdp.nyc.model.NTA;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -58,14 +60,18 @@ public class FXMLController {
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
 
+    private boolean grafoCreato = false ;
+    
     @FXML
     void doAnalisiArchi(ActionEvent event) {
-    	
-    	List<Arco> archi = model.analisiArchi();
-    	for(Arco a: archi) {
-    		txtResult.appendText(a.toString()+"\n");
+    	if(!this.grafoCreato) {
+    		txtResult.appendText("Devi prima creare il grafo\n");
+    		return ;
     	}
-
+    	List<Arco> archi = model.analisiArchi() ;
+    	for(Arco a: archi) {
+    		txtResult.appendText(a+"\n");
+    	}
     }
 
     @FXML
@@ -77,11 +83,44 @@ public class FXMLController {
     	}
     	
     	model.creaGrafo(borough);
+    	txtResult.setText("Grafo creato\n\n");
+    	this.grafoCreato = true ;
+    	
     }
 
     @FXML
     void doSimula(ActionEvent event) {
+    	
+    	if(!this.grafoCreato) {
+    		txtResult.appendText("Devi prima creare il grafo\n");
+    		return ;
+    	}
 
+    	
+    	String probS = txtProb.getText() ;
+    	String durationS = txtDurata.getText() ;
+    	
+    	if(probS.equals("") || durationS.equals("")) {
+    		txtResult.appendText("Errore: parametri obbligatori\n");
+    		return ;
+    	}
+    	
+    	double prob = 0.0 ;
+    	int duration = 0 ;
+
+    	try {
+	    	prob = Double.parseDouble(probS) ;
+	    	duration = Integer.parseInt(durationS) ;
+    	} catch(NumberFormatException e) {
+    		txtResult.appendText("Errore: inserire dati numerici\n");
+    		return ;
+    	}
+    	
+    	Map<NTA, Integer> condivisioni = model.simula(prob, duration);
+    	
+    	for(NTA n : condivisioni.keySet()) {
+    		txtResult.appendText("\n"+n.getNTACode()+ " "+ condivisioni.get(n)+"\n");
+    	}
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -97,14 +136,12 @@ public class FXMLController {
         assert txtDurata != null : "fx:id=\"txtDurata\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtProb != null : "fx:id=\"txtProb\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
-
-        
     }
     
     public void setModel(Model model) {
     	this.model = model;
     	List<String> boroughs = model.getBoroughs();
-    	this.cmbBorough.getItems().addAll(boroughs);
+    	cmbBorough.getItems().addAll(boroughs) ;
     }
 
 }
--- src/main/java/it/polito/tdp/nyc/model/Model.java
+++ src/main/java/it/polito/tdp/nyc/model/Model.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.jgrapht.Graph;
@@ -15,57 +16,65 @@ import it.polito.tdp.nyc.db.NYCDao;
 
 public class Model {
 	
-	private List<String> boroughs;
-	private List<NTA> NTAs;
-	private Graph<NTA, DefaultWeightedEdge> grafo;
+	private List<String> boroughs ;
+	private List<NTA> NTAs ;
+	private Graph<NTA, DefaultWeightedEdge> grafo ;
 	
-	public List<String> getBoroughs(){
+	public List<String> getBoroughs() {
 		if(this.boroughs==null) {
-			NYCDao dao = new NYCDao();
-			this.boroughs = dao.getAllBoroughs();
+			NYCDao dao = new NYCDao() ;
+			this.boroughs = dao.getAllBoroughs() ;
 		}
 		
-		return boroughs;
+		return this.boroughs ;
 	}
 	
 	public void creaGrafo(String borough) {
-		NYCDao dao = new NYCDao();
-		this.NTAs = dao.getNTAByBorough(borough);
+		NYCDao dao = new NYCDao() ;
+		this.NTAs = dao.getNTAByBorough(borough) ;
+//		System.out.println(this.NTAs) ;
 		
-		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
-		Graphs.addAllVertices(grafo, NTAs);
+		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class) ;
+		Graphs.addAllVertices(this.grafo, this.NTAs) ;
 		
-		for(NTA n1: NTAs) {
-			for(NTA n2: NTAs) {
-				if(!n1.equals(n2)) {
-					Set<String> unione = new HashSet<>(n1.getSSIDs());
-					unione.addAll(n2.getSSIDs());
-					Graphs.addEdge(grafo, n1, n2, unione.size());
+		for(NTA n1: this.NTAs) {
+			for(NTA n2: this.NTAs) {
+				if(n1.getNTACode().compareTo(n2.getNTACode())<0) { // !n1.equals(n2)
+					Set<String> unione = new HashSet<>(n1.getSSIDs()) ;
+					unione.addAll(n2.getSSIDs()) ;
+					Graphs.addEdge(this.grafo, n1, n2, unione.size()) ;
 				}
 			}
 		}
-		
-		System.out.println(grafo.vertexSet().size());
+		System.out.println("Vertici: "+this.grafo.vertexSet().size()+", Archi: "+this.grafo.edgeSet().size());	
 	}
 	
 	public List<Arco> analisiArchi() {
-		
-		double media = 0.0;
-		for(DefaultWeightedEdge e: this.grafo.edgeSet()){
-			media = media + this.grafo.getEdgeWeight(e);
+		double media = 0.0 ;
+		for(DefaultWeightedEdge e: this.grafo.edgeSet()) {
+			media = media + this.grafo.getEdgeWeight(e) ;
 		}
-		media = media/this.grafo.edgeSet().size();
+		media = media / this.grafo.edgeSet().size() ;
 		
 		List<Arco> result = new ArrayList<>();
 		for(DefaultWeightedEdge e: this.grafo.edgeSet()) {
 			if(this.grafo.getEdgeWeight(e)>media) {
-				result.add(new Arco(this.grafo.getEdgeSource(e).getNTACode(),
+				result.add(new Arco(
+						this.grafo.getEdgeSource(e).getNTACode(),
 						this.grafo.getEdgeTarget(e).getNTACode(),
-						(int)this.grafo.getEdgeWeight(e)));
+						(int)this.grafo.getEdgeWeight(e)
+						)) ;
 			}
 		}
 		
 		Collections.sort(result);
-		return result;
+		return result ;
+	}
+	
+	public Map<NTA, Integer> simula(double probShare, int durationShare) {
+		Simulatore sim = new Simulatore(this.grafo, probShare, durationShare) ;
+		sim.init();
+		sim.run();
+		return sim.getNumTotShare() ;
 	}
 }
